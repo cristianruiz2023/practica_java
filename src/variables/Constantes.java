89a1774206d7ccfdd745294d563a0ff0becbf6fd
Cristian Ruiz <cristianruizvalerod@gmail.com>
2024-01-30 12:08:12 -0300

    variables multiples cont

---
+++ src/variables/Constantes.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class Constantes {
+
+	public static void main(String[] args) {
+		
+		final float pi;
+		pi = 3.1415f;
+		
+		System.out.print(pi);
+
+	}
+
+}
---
+++ src/variables/VariableMultiple.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class VariableMultiple {
+
+	public static void main(String[] args) {
+//		int variable1;
+//		int variable2;
+//		int variable3;
+		
+		int variable1 = 1,variable2 = 2,variable3 = 3;
+		float pi = 3.1416f,e = 0.1010f;
+		
+//		variable1 = 1;
+//		variable2 = 2;
+//		variable3 = 3;
+		
+		System.out.println(variable1);
+		System.out.println(variable2);
+		System.out.println(variable3);
+		System.out.println(PI);
+		System.out.println(e);
+
+	}
+
+}
